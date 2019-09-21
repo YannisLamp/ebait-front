b3f2495b7cfdabee5d855f28a9171841fce60260
Yannis-Desktop <yann.lamprou@gmail.com>
2019-09-21 18:00:55 +0300

    correct message api

--- ted-react-redux-mat/src/services/messageApi.js
+++ ted-react-redux-mat/src/services/messageApi.js
@@ -1,6 +1,7 @@
 import axios from './axiosConfig';
 import handleError from './handleError';
 
+import { messageActions } from '../store/ducks/messageStore';
 
 const getAllContacts = async () => {
     return axios.get('/messages/allcontacts')
@@ -10,10 +11,20 @@ const getAllContacts = async () => {
         .catch(error => { handleError(error) });
 }
 
-
-const sendMessage = async (userId, message) => {
+const getNotifications = async () => {
+    return axios.get('/messages/notifs', {})
+    .then(
+        response => {
+            return response.data;
+        }
+    )
+    .catch(error => { handleError(error) });
+}
+ 
+const sendMessage = async (userId, subject, message) => {
     const jsonRequest = {
         userId,
+        subject,
         message
     }
 
@@ -21,6 +32,15 @@ const sendMessage = async (userId, message) => {
         .catch(error => { handleError(error) });
 }
 
+const getInboxAll = async () => {
+    return axios.get('/messages/inbox/all')
+        .then(
+            response => {
+                return response.data;
+            }
+        )
+        .catch(error => { handleError(error) });
+}
 
 const getInboxFrom = async (userId) => {
     return axios.get('/messages/inbox/' + userId)
@@ -32,9 +52,18 @@ const getInboxFrom = async (userId) => {
         .catch(error => { handleError(error) });
 }
 
-
 const getSentTo = async (userId) => {
     return axios.get('/messages/sent/' + userId)
+        .then(
+            response => {
+                return response.data;
+            }
+        )
+        .catch(error => { handleError(error) });
+}
+
+const getSentAll = async () => {
+    return axios.get('/messages/sent/all')
         .then(
             response => {
                 return response.data;
@@ -54,8 +83,32 @@ const deleteMessage = async (messageId) => {
         .catch(error => { handleError(error) });
 }
 
+const markAsReadMessage = async (messageId) => {
+    return axios.put('/messages/' + messageId, {})
+        .then(
+            response => {
+                return response.data;
+            }
+        )
+        .catch(error => { handleError(error) });
+}
+
+const refreshMessagesThunk = () => {
+    return dispatch => {
+        // get contacts
+        getAllContacts()
+            .then(contacts => {
+                messageActions
+            })
+
+        // get inbox
+
+    }
+}
+
 export const messageApi = {
     getAllContacts,
+    getNotifications,
 
     sendMessage,
     getInboxFrom,
