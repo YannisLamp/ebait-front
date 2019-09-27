a1ee666c18fad8f4658286a98f802e43bde50ac7
Yannis <yann.lamprou@gmail.com>
2019-09-27 15:28:27 +0300

    created confirm dialog

---
+++ ted-react-redux-mat/src/sharedComp/ConfirmDialog/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
+
+export default function ConfirmDialog(props) {
+    const { open, title, text }  = props;
+    const { handleCloseModal, confirmAction } = props; 
+
+    const closeAndDoAction = () => {
+        confirmAction();
+        handleCloseModal();
+    }
+
+    return (
+        <div>
+            <Dialog
+                open={open}
+                onClose={handleCloseModal}
+                aria-labelledby={"alert-dialog-title" + title}
+                aria-describedby={"alert-dialog-description" + title}
+            >
+                <DialogTitle id={"alert-dialog-title" + title}>{title}</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id={"alert-dialog-description" + title}>
+                        {text}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleCloseModal} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={closeAndDoAction} color="primary" autoFocus>
+                        Confirm
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
